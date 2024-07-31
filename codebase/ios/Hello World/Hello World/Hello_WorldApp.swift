17d8f0e2f1ce2bc893346d671c1b4e87fef54c06
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-07-31 23:33:34 +0530

    [TECH] added ios hello world project

---
+++ codebase/ios/Hello World/Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Abhimanyu on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
